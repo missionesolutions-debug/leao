3c43ed30e7868bf17b1e98c1e2b2a01486ecdc62
Podearroz <missionesolutions@gmail.com>
2025-09-09 13:18:28 -0300

    colocando as rotas do backend no front

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DEBUG: bool = False
-    ALLOW_ORIGINS: str = "*"
+    ALLOW_ORIGINS: str = ["http://localhost:3000"]
 
     class Config:
         env_file = "app/.env"
